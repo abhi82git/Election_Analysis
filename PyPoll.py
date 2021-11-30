ea78a6f3a9466dba8ea848069cc55f55bf04bd35
Abhishek Dixit <abhisheksmailbox@gmail.com>
2021-11-30 03:13:28 -0500

    Calculated total votes secured by each candidate

--- PyPoll.py
+++ PyPoll.py
@@ -12,8 +12,20 @@ import csv
 import os
 
 
-# Assign a file to a variable
+# Assign input file to a variable
 file_to_load = os.path.join("Resources","election_results.csv")
+
+# Assign output file to variable
+file_to_save = os.path.join("Analysis","election_analysis.txt")
+
+# Total Votes Counter
+Total_Votes = 0
+
+# Initialize an empty list to later hold unique candidate names
+Candidate_Name_List = []
+
+# Initialize an empty dictionary to later hold ballots for each candidate
+Candidate_Ballot_Dict = {}
 
 with open(file_to_load) as election_data:
 
@@ -22,13 +34,20 @@ with open(file_to_load) as election_data:
 
     # Print the header rows
     headers = next(file_reader)
-    print(headers)
 
-    # for row in file_reader:
-      # print(row[0])
+    for row in file_reader:
+      Total_Votes += 1
+      Candidate_Name = row[2]
+      if Candidate_Name not in Candidate_Name_List:
+          Candidate_Name_List.append(Candidate_Name)
+          Candidate_Ballot_Dict[Candidate_Name] = 0
+          print(f"Candidate in election: {Candidate_Name}")
 
-# Assign output file to variable
-file_to_save = os.path.join("Analysis","election_analysis.txt")
+      Candidate_Ballot_Dict[Candidate_Name] += 1
+
+    print(f"\nTotal votes cast in the election: {Total_Votes}\n")
+    for key,value in Candidate_Ballot_Dict.items():
+      print(f"{key} secured {value} votes.\n")
 
 with open(file_to_save,"w") as election_output:
 
