1f5e64aa5bade25db7f30508744192c34f8bcc9e
Abhishek Dixit <abhisheksmailbox@gmail.com>
2021-12-01 13:44:16 -0500

    Completed Code Base

--- PyPoll.py
+++ PyPoll.py
@@ -21,17 +21,29 @@ file_to_save = os.path.join("Analysis","election_analysis.txt")
 # Total Votes Counter
 Total_Votes = 0
 
-# Percent Votes Counter
-Percent_Votes = 0.00
+# County Percent Votes Initialization
+County_Percent_Votes = 0.0
+
+# Candidate Percent Votes Counter
+Candidate_Percent_Votes = 0.0
 
 # Winning Vote Counter
 Winner_Vote_count = 0
 
+# Biggest turnout county counter
+Biggest_Turnout_County_count = 0
+
 # Initialize an empty list to later hold unique candidate names
 Candidate_Name_List = []
 
+# Initialize an empty list to later hold unique county names
+County_Name_List = []
+
 # Initialize an empty dictionary to later hold ballots for each candidate
 Candidate_Ballot_Dict = {}
+
+# Initialize an empty dictionary to later hold ballots for each county
+County_Ballot_Dict = {}
 
 with open(file_to_load) as election_data:
 
@@ -44,12 +56,19 @@ with open(file_to_load) as election_data:
     for row in file_reader:
       Total_Votes += 1
       Candidate_Name = row[2]
+      County_Name = row[1]
       if Candidate_Name not in Candidate_Name_List:
           Candidate_Name_List.append(Candidate_Name)
           Candidate_Ballot_Dict[Candidate_Name] = 0
           print(f"Candidate in election: {Candidate_Name}")
 
+      if County_Name not in County_Name_List:
+          County_Name_List.append(County_Name)
+          County_Ballot_Dict[County_Name] = 0
+          print(f"County in Election: {County_Name}")
+
       Candidate_Ballot_Dict[Candidate_Name] += 1
+      County_Ballot_Dict[County_Name] += 1
 
     with open(file_to_save, "w") as election_output:
 
@@ -63,15 +82,39 @@ with open(file_to_load) as election_data:
         election_output.write(Election_Summary)
         print(Election_Summary)
 
+        County_Summary = (
+            f"--------------------------------------------\n"
+            f"County Votes:\n"
+        )
+        election_output.write(County_Summary)
+        print(County_Summary)
+
+        for key, value in County_Ballot_Dict.items():
+            County_Percent_Votes = (value / Total_Votes) * 100
+            County_Votes_Summary = (f"{key}: {County_Percent_Votes:.1f} % ({value:,}).\n")
+            print(County_Votes_Summary)
+            election_output.write(County_Votes_Summary)
+            if value > Biggest_Turnout_County_count:
+                Biggest_Turnout_County_count = value
+                Biggest_Turnout_County = key
+                Biggest_Turnout_County_Percent = County_Percent_Votes
+        Biggest_Turnout_County_Summary = (
+            f"---------------------------------------------\n"
+            f"Largest County Turnout: {Biggest_Turnout_County}\n"
+            f"---------------------------------------------\n"
+        )
+        election_output.write(Biggest_Turnout_County_Summary)
+        print(Biggest_Turnout_County_Summary)
+
         for key, value in Candidate_Ballot_Dict.items():
-            Percent_Votes = (value / Total_Votes) * 100
-            Votes_Summary = (f"{key}: {Percent_Votes:.2f} % ({value:,}).\n")
-            print(Votes_Summary)
-            election_output.write(Votes_Summary)
+            Candidate_Percent_Votes = (value / Total_Votes) * 100
+            Candidate_Votes_Summary = (f"{key}: {Candidate_Percent_Votes:.1f} % ({value:,}).\n")
+            print(Candidate_Votes_Summary)
+            election_output.write(Candidate_Votes_Summary)
             if value > Winner_Vote_count:
                 Winner_Vote_count = value
                 Winner = key
-                Winner_Percent = Percent_Votes
+                Winner_Percent = Candidate_Percent_Votes
 
         # Write the winner and vote count to output file
         Winning_Candidate_Summary = (
@@ -80,7 +123,7 @@ with open(file_to_load) as election_data:
             f"---------------------------------------------\n"
             f"Winner: {Winner}\n"
             f"Winning Vote Count: {Winner_Vote_count:,}\n"
-            f"Winning Percentage: {Winner_Percent:.2f} %\n"
+            f"Winning Percentage: {Winner_Percent:.1f} %\n"
             f"---------------------------------------------\n"
         )
         election_output.write(Winning_Candidate_Summary)
